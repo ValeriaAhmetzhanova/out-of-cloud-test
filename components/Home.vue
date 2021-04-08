a55058717ccf78c98924205d1bcae2c810f2fd76
Valeria <v.ahmetzhanova@gmail.com>
2021-04-08 15:40:50 +0800

    add home component

---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template><div class="home"></div></template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      popupActive: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  /*to check scrolling*/
+  height: 200vh;
+}
+</style>
